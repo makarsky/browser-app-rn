35443af24046b320ee5bf459daffe3cb9369d446
Ihar Makarski <22708426+makarsky@users.noreply.github.com>
2020-11-19 21:07:07 +0000

    Set up react-native-paper and react-native-vector-icons

--- index.js
+++ index.js
@@ -2,8 +2,26 @@
  * @format
  */
 
+import * as React from 'react';
 import {AppRegistry} from 'react-native';
+import { DefaultTheme, Colors, Provider as PaperProvider } from 'react-native-paper';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: Colors.blue500,
+    },
+};
+
+export default function Main() {
+    return (
+      <PaperProvider theme={theme}>
+        <App/>
+      </PaperProvider>
+    );
+  }
+
+AppRegistry.registerComponent(appName, () => Main);
