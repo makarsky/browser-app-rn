4515dd3f1ba1f9ffe2f53fecebaa9d2bd98a58f9
Igor Makarsky <makarrski@gmail.com>
2020-11-21 09:17:56 +0000

    Add Appbar and BottomNavigation

--- App.js
+++ App.js
@@ -7,9 +7,16 @@
  */
 
 import * as React from 'react';
-import { Appbar } from 'react-native-paper';
+import { Appbar, BottomNavigation, Text } from 'react-native-paper';
 
-function App() {
+
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => <Text>Albums</Text>;
+
+const RecentsRoute = () => <Text>Recents</Text>;
+
+const App = () => {
 
   const _goBack = () => console.log('Went back');
 
@@ -17,13 +24,35 @@ function App() {
 
   const _handleMore = () => console.log('Shown more');
 
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'goBack', title: 'Music', icon: 'album' },
+    { key: 'goForward', title: 'Albums', icon: 'album' },
+    { key: 'bookmark', title: 'Recents', icon: 'bookmark' },
+    { key: 'bookmarks', title: '', icon: 'history' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    goBack: MusicRoute,
+    goForward: AlbumsRoute,
+    bookmark: RecentsRoute,
+    bookmarks: RecentsRoute,
+  });
+
   return (
-    <Appbar.Header>
-      <Appbar.BackAction onPress={_goBack} />
-      <Appbar.Content title="Title" subtitle="Subtitle" />
-      <Appbar.Action icon="reload" onPress={_handleSearch} />
-      <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
-    </Appbar.Header>
+    <>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={_goBack} />
+        <Appbar.Content title="Title" subtitle="Subtitle" />
+        <Appbar.Action icon="reload" onPress={_handleSearch} />
+        <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+      </Appbar.Header>
+      <BottomNavigation
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+      />
+    </>
   );
 };
 
