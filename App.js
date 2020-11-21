0fd1bcae248a801b75f11bd3a06ce7c2f1bda108
Ihar Makarski <22708426+makarsky@users.noreply.github.com>
2020-11-21 12:17:39 +0000

    Implement basic browser layout

--- App.js
+++ App.js
@@ -7,51 +7,66 @@
  */
 
 import * as React from 'react';
-import {Appbar, BottomNavigation, Text} from 'react-native-paper';
-
-const MusicRoute = () => <Text>Music</Text>;
-
-const AlbumsRoute = () => <Text>Albums</Text>;
-
-const RecentsRoute = () => <Text>Recents</Text>;
+import {Appbar, BottomNavigation, Text, Colors} from 'react-native-paper';
+import { StyleSheet, View, TextInput } from 'react-native';
 
 const App = () => {
-  const _goBack = () => console.log('Went back');
+  const [currentUrl, setCurrentUrl] = React.useState('https://google.com/aaaaaaaaaaaaaaaaaaaauhviuviuviuvuviuviuviviviyviyvyv');
 
-  const _handleSearch = () => console.log('Searching');
+  const _handleReload = () => console.log('Reload');
+
+  const _handleLoadUrl = () => console.log('Load URL');
 
   const _handleMore = () => console.log('Shown more');
 
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    {key: 'goBack', title: 'Music', icon: 'album'},
-    {key: 'goForward', title: 'Albums', icon: 'album'},
-    {key: 'bookmark', title: 'Recents', icon: 'bookmark'},
-    {key: 'bookmarks', title: '', icon: 'history'},
-  ]);
+  const _handleGoBack = () => console.log('Go back');
 
-  const renderScene = BottomNavigation.SceneMap({
-    goBack: MusicRoute,
-    goForward: AlbumsRoute,
-    bookmark: RecentsRoute,
-    bookmarks: RecentsRoute,
-  });
+  const _handleGoForward = () => console.log('Go forward');
+
+  const _handleBoormark = () => console.log('Make a bookmark');
+
+  const _handleBookmarkList = () => console.log('List');
 
   return (
-    <>
+    <View style={styles.view}>
       <Appbar.Header>
-        <Appbar.BackAction onPress={_goBack} />
-        <Appbar.Content title="Title" subtitle="Subtitle" />
-        <Appbar.Action icon="reload" onPress={_handleSearch} />
+        <TextInput style={styles.urlInput} value={currentUrl} onChangeText={setCurrentUrl} />
+        <Appbar.Action icon="arrow-right-bold" onPress={_handleLoadUrl} />
+        <Appbar.Action icon="reload" onPress={_handleReload} />
         <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
       </Appbar.Header>
-      <BottomNavigation
-        navigationState={{index, routes}}
-        onIndexChange={setIndex}
-        renderScene={renderScene}
-      />
-    </>
+      <Appbar style={styles.bottomAppbar}>
+        <Appbar.Action icon="chevron-left" onPress={_handleGoBack} disabled={true} />
+        <Appbar.Action icon="chevron-right" onPress={_handleGoForward} />
+        <Appbar.Action icon="bookmark-outline" onPress={_handleBoormark} />
+        <Appbar.Action icon="format-list-bulleted" onPress={_handleBookmarkList} />
+      </Appbar>
+    </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  urlInput: {
+    flex: 1,
+    height: 40,
+    borderRadius: 24,
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    backgroundColor: '#ffffff',
+    paddingHorizontal: 12,
+    marginHorizontal: 6,
+  },
+  bottomAppbar: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "space-evenly",
+    backgroundColor: Colors.grey100
+  },
+  view: {
+    flex: 1,
+  },
+});
