a8c2208b04bb4be25f06d97c435eed6c3dafd0bb
Igor Makarsky <makarrski@gmail.com>
2020-11-21 09:52:17 +0000

    Apply lint-fix

--- index.js
+++ index.js
@@ -4,24 +4,28 @@
 
 import * as React from 'react';
 import {AppRegistry} from 'react-native';
-import { DefaultTheme, Colors, Provider as PaperProvider } from 'react-native-paper';
+import {
+  DefaultTheme,
+  Colors,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 import App from './App';
 import {name as appName} from './app.json';
 
 const theme = {
-    ...DefaultTheme,
-    colors: {
-      ...DefaultTheme.colors,
-      primary: Colors.blue500,
-    },
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: Colors.blue500,
+  },
 };
 
 export default function Main() {
-    return (
-      <PaperProvider theme={theme}>
-        <App/>
-      </PaperProvider>
-    );
-  }
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
 
 AppRegistry.registerComponent(appName, () => Main);
